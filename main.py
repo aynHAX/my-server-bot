c828e59b13c42faf01f1cf562469256826137fc3
aynHAX <aymenouksel111@gmail.com>
2026-02-19 22:32:17 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 import telebot
 import os
 
-# جلب التوكن من متغيرات البيئة لحمايته
 BOT_TOKEN = os.environ.get('BOT_TOKEN')
 bot = telebot.TeleBot(BOT_TOKEN)
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.reply_to(message, "أهلاً بك! البوت يعمل بنجاح على منصة Koyeb 🚀")
+    bot.reply_to(message, "أهلاً بك! البوت يعمل بنجاح باستخدام Docker على منصة Koyeb 🐳🚀")
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
